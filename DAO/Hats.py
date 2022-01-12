fb8f9c4e8b5e35ec24386f95a96dc40b7ffff49a
Ron Levi <ron.levi@appsflyer.com>
2022-01-12 15:15:33 +0200

    DAO, DTO design

---
+++ DAO/Hats.py
@@ -0,0 +1,6 @@
+class Hats:
+    def __init__(self, db_con):
+        self.db_con = db_con
+
+    def insert(self):
+        pass
---
+++ Repository.py
@@ -0,0 +1,10 @@
+class Repository:
+    def __init__(self):
+        pass
+
+    def create_tables(self):
+        pass
+
+    def _close(self):
+        pass
+
