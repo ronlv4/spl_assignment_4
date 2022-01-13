638aa653f07233a9dc150b7456539be99ed85f0c
Ron Levi <ron.levi@appsflyer.com>
2022-01-13 20:36:34 +0200

    refactored create tables

--- Repository.py
+++ Repository.py
@@ -17,31 +17,23 @@ class _Repository:
         self._conn.close()
 
     def create_tables(self):
-        # db_cursor.execute(
-        #     "CREATE TABLE hats(ID INTEGER PRIMARY KEY,topping VARCHAR NOT NULL, supplier INTEGER REFERENCES Supplier(id), quantity INTEGER NOT NULL);")
-        # db_cursor.execute("CREATE TABLE suppliers(ID INTEGER PRIMARY KEY,name VARCHAR NOT NULL);")
-        # db_cursor.execute(
-        #     "CREATE TABLE orders(ID INTEGER PRIMARY KEY,location VARCHAR NOT NULL, hat INTEGER REFERENCES hats(id));")
         self._conn.executescript("""
-        CREATE TABLE students (
-            id      INT         PRIMARY KEY,
-            name    TEXT        NOT NULL
+        CREATE TABLE hats (
+            ID          INTEGER         PRIMARY KEY,
+            topping     VARCHAR         NOT NULL,
+            supplier    INTEGER         REFERENCES Supplier(id),
+            quantity    INTEGER         NOT NULL
         );
 
-        CREATE TABLE assignments (
-            num                 INT     PRIMARY KEY,
-            expected_output     TEXT    NOT NULL
+        CREATE TABLE suppliers(
+            ID          INTEGER         PRIMARY KEY,
+            name        VARCHAR         NOT NULL
         );
 
-        CREATE TABLE grades (
-            student_id      INT     NOT NULL,
-            assignment_num  INT     NOT NULL,
-            grade           INT     NOT NULL,
-
-            FOREIGN KEY(student_id)     REFERENCES students(id),
-            FOREIGN KEY(assignment_num) REFERENCES assignments(num),
-
-            PRIMARY KEY (student_id, assignment_num)
+        CREATE TABLE orders (
+            ID          INTEGER         PRIMARY KEY,
+            location    VARCHAR         NOT NULL,
+            hat         INTEGER         REFERENCES hats(ID)
         );
     """)
 
